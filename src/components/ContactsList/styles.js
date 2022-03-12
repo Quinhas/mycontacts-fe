29d7c4af58746273e6d919a043175c95e26feb5b
Quinhas <ln.santana08@gmail.com>
2022-03-12 12:46:14 -0300

    Criando o header do ContactsList

---
+++ src/components/ContactsList/styles.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin-top: 2rem;
+`;
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  strong {
+    color: #222;
+    font-size: 1.5rem;
+  }
+
+  a {
+    color: ${({ theme }) => theme.colors.primary.main};
+    text-decoration: none;
+    font-weight: bold;
+    padding: .5rem 1rem;
+    border: 2px solid ${({ theme }) => theme.colors.primary.main};
+    border-radius: 4px;
+    transition: all 0.2s ease-in;
+
+    &:hover {
+      background-color: ${({ theme }) => theme.colors.primary.lighter};
+    }
+
+    &:active {
+      background-color: ${({ theme }) => theme.colors.primary.light};
+      color: #fff;
+    }
+  }
+`;
