b2495e14a3a2a31104ac85ac64ce53726aa4ba06
Quinhas <ln.santana08@gmail.com>
2022-03-12 13:28:35 -0300

    Criando a listagem de contatos

--- src/components/ContactsList/styles.js
+++ src/components/ContactsList/styles.js
@@ -10,7 +10,6 @@ export const Header = styled.header`
   justify-content: space-between;
 
   strong {
-    color: #222;
     font-size: 1.5rem;
   }
 
@@ -30,6 +29,74 @@ export const Header = styled.header`
     &:active {
       background-color: ${({ theme }) => theme.colors.primary.light};
       color: #fff;
+    }
+  }
+`;
+
+export const ListContainer = styled.div`
+  margin-top: 1.5rem;
+
+  header {
+    margin-bottom: 0.5rem;
+    button {
+      background: transparent;
+      border: none;
+      display: flex;
+      align-items: center;
+
+      span {
+        margin-right: 0.5rem;
+        font-weight: bold;
+        color: ${({ theme }) => theme.colors.primary.main};
+      }
+    }
+  }
+`;
+
+export const Card = styled.div`
+  background: #fff;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  padding: 1rem;
+  border-radius: .25rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  & + & {
+    margin-top: 1rem;
+  }
+
+  .info {
+    .contact-name {
+      display: flex;
+      align-items: center;
+
+      small {
+        background: ${({ theme }) => theme.colors.primary.lighter};
+        color: ${({ theme }) => theme.colors.primary.light};
+        font-weight: bold;
+        text-transform: uppercase;
+        padding: .25rem;
+        border-radius: .25rem;
+        margin-left: 0.5rem;
+      }
+    }
+
+    span {
+      display: block;
+      font-size: .875rem;
+      color: ${({ theme }) => theme.colors.gray[200]};
+    }
+  }
+
+  .actions {
+    display: flex;
+    align-items: center;
+
+    button {
+      background: transparent;
+      border: none;
+      margin-left: .5rem;
     }
   }
 `;
