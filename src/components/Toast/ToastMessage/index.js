bbd98416c1fcd6070ba2513e378aba28385fe0d7
Lucas Santana <ln.santana08@gmail.com>
2022-07-19 19:38:28 -0300

    Trabalhando com variantes no Styled Components

--- src/components/Toast/ToastMessage/index.js
+++ src/components/Toast/ToastMessage/index.js
@@ -6,7 +6,7 @@ import xCircleIcon from '../../../assets/images/icons/x-circle.svg';
 
 export default function ToastMessage({ text, type }) {
   return (
-    <Container>
+    <Container type={type}>
       {type === 'error' && <img src={xCircleIcon} alt="X" />}
       {type === 'success' && <img src={checkCircleIcon} alt="Check" />}
       <strong>
--- src/components/Toast/ToastMessage/styles.js
+++ src/components/Toast/ToastMessage/styles.js
@@ -1,8 +1,13 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+const containerVariants = {
+  default: css`background: ${({ theme }) => theme.colors.primary.main};`,
+  success: css`background: ${({ theme }) => theme.colors.success.main};`,
+  error: css`background: ${({ theme }) => theme.colors.danger.main};`,
+};
 
 export const Container = styled.div`
   padding: 1rem 2rem;
-  background: ${({ theme }) => theme.colors.primary.main};
   color: #fff;
   border-radius: 4px;
   box-shadow: 0rem 1.25rem 1.25rem -1rem rgba(0, 0, 0, 0.25);
@@ -10,6 +15,8 @@ export const Container = styled.div`
   align-items: center;
   justify-content: center;
   gap: 0.5rem;
+
+  ${({ type }) => containerVariants[type] || containerVariants.default}
 
   & + & {
     margin-top: 12px;
